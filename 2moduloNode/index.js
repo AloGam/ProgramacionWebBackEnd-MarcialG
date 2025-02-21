b6011ef8618701e1f5d58443f15935df46244160
AloGam <alegamhu@gmail.com>
2025-02-20 19:25:32 -0500

    Finalizacion carpeta 2 y 3

---
+++ 2moduloNode/index.js
@@ -0,0 +1,20 @@
+const { isUtf8 } = require("buffer");
+const fs = require("fs");
+
+//generar archivo.txt con contenido
+/*
+fs.writeFile("archivo.txt", "prueba del archivo de texto", (err)=>{
+    if(err)throw err;
+    console.log("El archivo se ha creado");
+});
+*/
+
+//Imprimir el contenido del archivo txt en la terminal
+/*
+fs.readFile('./archivo.txt', 'Utf8',(err, data) => {
+    if (err) throw err;
+    console.log(data);
+  });
+  */
+
+  
