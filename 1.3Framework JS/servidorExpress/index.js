b4e2c808c4424394a998603551ba8b6e4169e34e
AloGam <alegamhu@gmail.com>
2025-02-27 21:35:37 -0500

    Ejercicios de shell

--- 1.3Framework JS/servidorExpress/index.js
+++ 1.3Framework JS/servidorExpress/index.js
@@ -1,15 +1,23 @@
 import express from 'express';
+import path from 'path';
+
 const app = express();
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, 'views')));
+
 app.get('/', (req, res) => {
     res.send('Hola');
-    })
+    });
 
 app.get('/about', (req, res) => {
     res.send('hola');
-    })
+    });
+
+app.get('/contact', (req, res) => {
+    res.send('hola');
+    });
 
 app.listen(port, () => {
-    console.log(`Server ejecutandose en puerto ${port}`);
-    })
+    console.log(`Server ejecutandose en puerto 3000`);
+    });
