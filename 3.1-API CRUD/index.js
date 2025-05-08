aeb95ea66279886b2e40fe5d8e910669e3e62ce0
AloGam <alegamhu@gmail.com>
2025-05-08 15:50:53 -0500

    AA 3.1 Proyecto CRUD API

---
+++ 3.1-API CRUD/index.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import Usuario from './models/usuario.model.js';
+
+dotenv.config();
+
+const app = express();
+const puerto = 3000;
+const uri = process.env.uri;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.post('/usuarios', async (req, res) => {
+  try {
+    const usuario = await Usuario.create(req.body);
+    res.status(201).json(usuario);
+  } catch (error) {
+    console.error("Error al crear el usuario:", error);
+    res.status(500).json({ error: 'Error al crear el usuario' });
+  }
+});
+
+app.get('/', (req, res) => {
+  res.send('Bienvenido a mi API CRUD');
+});
+
+app.listen(puerto, () => {
+  console.log(`Servidor corriendo en http://localhost:${puerto}`);
+});
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Conexión exitosa a la base de datos');
+  })
+  .catch((error) => {
+    console.error('Error al conectar a la base de datos:', error);
+  });
