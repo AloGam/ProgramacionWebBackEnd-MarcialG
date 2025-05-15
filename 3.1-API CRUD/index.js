ebfd046a59f5c5432373053ba2fc21651388c06b
AloGam <alegamhu@gmail.com>
2025-05-14 23:40:15 -0500

    AA 3.2 Proyecto CRUD API

--- 3.1-API CRUD/index.js
+++ 3.1-API CRUD/index.js
@@ -23,6 +23,57 @@ app.post('/usuarios', async (req, res) => {
   }
 });
 
+app.put('/usuarios/:id', async (req, res) => {
+  try {
+    const {id} = req.params;
+    const usuario = await Usuario.findByIdAndUpdate(id, req.body);
+    if (!usuario) {
+      return res.status(404).json({ error: 'Usuario no encontrado' });
+    }
+    const usuarioActualizado = await Usuario.findById(id);
+    res.status(200).json(usuarioActualizado);
+    console.log(usuarioActualizado);
+  } catch (error) {
+    console.error("Error al actualizar el usuario:", error);
+    res.status(500).json({ error: 'Error al actualizar el usuario' });
+  }
+});
+
+app.delete('/usuarios/:_id', async (req, res) => {
+  try {
+    const {_id} = req.params;
+    const usuario = await Usuario.findByIdAndDelete(_id);
+    if (!usuario) {
+      return res.status(404).json({ error: 'Usuario no encontrado' });
+    }
+    res.status(200).json({ message: 'Usuario eliminado' });
+  } catch (error) {
+    console.error("Error al eliminar el usuario:", error);
+    res.status(500).json({ error: 'Error al eliminar el usuario' });
+  }
+});
+
+app.get('/usuarios', async (req, res) => {
+  try {
+    const usuarios = await Usuario.find();
+    res.status(200).json(usuarios);
+  } catch (error) {
+    console.error("Error al obtener los usuarios:", error);
+    res.status(500).json({ error: 'Error al obtener los usuarios' });
+  }
+});
+
+app.get('/usuarios/:id', async (req, res) => {
+  try {
+    const {id} = req.params;
+    const usuario = await Usuario.findById(id);
+    res.status(200).json(usuario);
+  } catch (error) {
+    console.error("Error al obtener el usuario:", error);
+    res.status(500).json({ error: 'Error al obtener el usuario' });
+  }
+});
+
 app.get('/', (req, res) => {
   res.send('Bienvenido a mi API CRUD');
 });
@@ -31,7 +82,7 @@ app.listen(puerto, () => {
   console.log(`Servidor corriendo en http://localhost:${puerto}`);
 });
 
-mongoose.connect(uri)
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Conexión exitosa a la base de datos');
   })
